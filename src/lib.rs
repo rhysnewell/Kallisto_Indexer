435740e3df187374d662e393947fd659f2578a56
Rhys Newell <rhys.newell@uqconnect.edu.au>
2018-09-30 14:54:49 +1000

    First build of indexer

---
+++ src/lib.rs
@@ -0,0 +1,73 @@
+// pub mod contig;
+// pub mod genome;
+// pub mod mosdepth_genome_coverage_estimators;
+pub mod genomes_and_contigs;
+// pub mod bam_generator;
+// pub mod filter;
+// pub mod external_command_checker;
+// pub mod bwa_index_maintenance;
+
+extern crate bio;
+extern crate log;
+
+extern crate rust_htslib;
+extern crate env_logger;
+extern crate nix;
+extern crate tempdir;
+
+use std::path::Path;
+use genomes_and_contigs::GenomesAndContigs;
+
+
+
+pub fn read_genome_fasta_files(fasta_file_paths: &[&str]) -> GenomesAndContigs {
+    let mut contig_to_genome = GenomesAndContigs::new();
+
+    for file in fasta_file_paths {
+        let path = Path::new(*file);
+        let reader = bio::io::fasta::Reader::from_file(path)
+            .expect(&format!("Unable to read fasta file {}", file));
+
+        let genome_name = String::from(
+            path.file_stem()
+                .expect("Problem while determining file stem")
+            .to_str()
+                .expect("File name string conversion problem"));
+        if contig_to_genome.genome_index(&genome_name).is_some() {
+            panic!("The genome name {} was derived from >1 file", genome_name);
+        }
+        let genome_index = contig_to_genome.establish_genome(genome_name);
+        for record in reader.records() {
+            let contig = String::from(
+                record
+                    .expect(&format!("Failed to parse contig name in fasta file {:?}", path))
+                    .id());
+            contig_to_genome.insert(contig, genome_index);
+        }
+    }
+    return contig_to_genome;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_contig_to_genome(){
+        let mut contig_to_genome = GenomesAndContigs::new();
+        let genome = String::from("genome0");
+        let index = contig_to_genome.establish_genome(genome);
+        contig_to_genome.insert(String::from("contig1"), index);
+        assert_eq!(
+            String::from("genome0"),
+            *(contig_to_genome.genome_of_contig(&String::from("contig1")).unwrap()));
+    }
+
+    #[test]
+    fn test_read_genome_fasta_files_one_genome(){
+        let contig_to_genome = read_genome_fasta_files(&vec!["tests/data/genome1.fna"]);
+        assert_eq!(String::from("genome1"), *contig_to_genome.genome_of_contig(&String::from("seq1")).unwrap());
+        assert_eq!(String::from("genome1"), *contig_to_genome.genome_of_contig(&String::from("seq2")).unwrap());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,85 @@
+extern crate kallisto_indexer;
+
+extern crate bio;
+extern crate clap;
+use clap::*;
+extern crate rust_htslib;
+extern crate nix;
+extern crate tempdir;
+extern crate csv;
+
+extern crate log;
+use log::LogLevelFilter;
+extern crate env_logger;
+use env_logger::LogBuilder;
+
+use std::env;
+use std::str;
+use std::process;
+use std::path::Path;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut app = build_cli();
+    let m = app.clone().get_matches();
+    let genomes_and_contigs;
+    if m.is_present("fasta-files"){
+        let genome_fasta_files: Vec<&str> = m.values_of("fasta-files").unwrap().collect();
+        genomes_and_contigs = kallisto_indexer::read_genome_fasta_files(&genome_fasta_files);
+    } else if m.is_present("fasta-directory") {
+        let file_path = m.value_of("fasta-directory").unwrap();
+        let mut rdr = csv::Reader::from_path(file_path);
+        let mut genome_fasta_files: Vec<String> = vec!();
+        for line in rdr.unwrap().records() {
+            let record = line.unwrap();
+            let file = &record[1];
+            let mut s = String::from(file);
+            genome_fasta_files.push(s);
+        }
+        let mut strs: Vec<&str> = vec!();
+        for f in &genome_fasta_files {
+            strs.push(f);
+        }
+        genomes_and_contigs = kallisto_indexer::read_genome_fasta_files(&strs);
+        let mut genomes_string = String::new();
+        for (key, value) in genomes_and_contigs.contig_to_genome{
+            genomes_string = [genomes_string, key].join("N");
+        }
+
+    }
+}
+
+fn build_cli() -> App<'static, 'static> {
+
+    return App::new("kallisto_indexer")
+        .author("Rhys J. P. Newell <r.newell near uq.edu.au>")
+        .about("Mapping coverage analysis for metagenomics")
+        .args_from_usage("-v, --verbose       'Print extra debug logging information'
+             -q, --quiet         'Unless there is an error, do not print logging information'")
+        .help("")
+        .arg(Arg::with_name("fasta-files")
+                .short("f")
+                .long("fasta-files")
+                .conflicts_with("fasta-directory")
+                .multiple(true)
+                .takes_value(true))
+        .arg(Arg::with_name("threads")
+                .short("-t")
+                .long("threads")
+                .default_value("1")
+                .takes_value(true))
+        .arg(Arg::with_name("fasta-directory")
+                .short("d")
+                .long("fasta-directory")
+                .conflicts_with("fasta-files")
+                .takes_value(true))
+        .arg(Arg::with_name("verbose")
+                .short("v")
+                .long("verbose"))
+        .arg(Arg::with_name("quiet")
+                .short("q")
+                .long("quiet"))
 }
